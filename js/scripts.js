c69e4a212eb32bf996962db465d1b8e6a65d8497
Zahin <azahinhasan@gmail.com>
2020-11-04 14:56:27 +0600

    a

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,9 @@
 function myFunction() {
     var value = document.getElementById("text").value;
     var totalSoFar=0;
-    var spaceCount=0;
+    var spaceCount=0; 
+    var sentencesCount=0;
+    //var re = /[.!?]/;
     if(value.length != 0){
         totalSoFar=1;
     }
@@ -12,9 +14,17 @@ function myFunction() {
         if (value.charAt(i) == ' ') { 
             spaceCount++; 
         }
+       // var numOfSentences = value.split(re);
+       if (value.charAt(i) == '.' || value.charAt(i) == '!' ||value.charAt(i) == '?') { 
+           if(value.charAt(i-1) != '.' || value.charAt(i-1) != '?'|| value.charAt(i-1) != '!' ){
+                sentencesCount++; 
+           }
+            
+        }
     }
 
     document.getElementById("word").innerHTML = "Total Words : "+totalSoFar;
     document.getElementById("charactersWithSpace").innerHTML = "Total Characters(with spaces) : "+ value.length;
     document.getElementById("letter").innerHTML = "Total Characters(no spaces) : "+ (value.length-spaceCount);
+    document.getElementById("sentences").innerHTML = "Total Sentences : "+ sentencesCount;
 }
